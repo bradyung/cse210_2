63e340550071e829806dce03f35830474a4faf2a
bradyung <ybrad1109@gmail.com>
2026-03-05 09:41:20 -0700

    base menu working class.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,6 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Menu menu = new Menu();
-        menu.ProcessMenu();
+        bool done = false;
+        int userResponse;
+        do
+        {
+            userResponse = menu.ProcessMenu();
+            switch (userResponse)
+            {
+                case 1:
+                    // Create a new journal entry here
+                    // add entry to list of journal entries
+                    break;
+                case 2:
+                    // Display journal entries here
+                    break;
+                case 3:
+                    // save journal to file
+                    break;
+                case 4:
+                    // load journal from file
+                    break;
+                case 5:
+                    done = true;
+                    break;
+            }
+        } while (!done);
     }
 }
+        
