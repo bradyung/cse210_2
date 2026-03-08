2d521e5dd1ae7e17ef369648bc312aa866a4d904
bradyung <ybrad1109@gmail.com>
2026-03-07 23:08:54 -0700

    Running, but not completly finished

--- prove/Develop02/Menu.cs
+++ prove/Develop02/Menu.cs
@@ -25,7 +25,9 @@ class Menu
             {
                 Console.WriteLine(line);
             }
+            Console.WriteLine("Choose an option: ");
             userResponse = int.Parse(Console.ReadLine());
+            
         } while(userResponse < 1 || userResponse > 5);
 
         
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,6 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Menu menu = new Menu();
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
         bool done = false;
         int userResponse;
         do
@@ -14,16 +16,51 @@ class Program
             {
                 case 1:
                     // Create a new journal entry here
+                    string prompt = promptGenerator.GetRandomPrompt();
+                    Console.WriteLine();
+                    Console.WriteLine($"Prompt: {prompt}");
+                    Console.WriteLine("Your response:");
+                    string response = Console.ReadLine();
+
+                    Entry newEntry = new Entry();
+                    newEntry._date = DateTime.Now.ToShortDateString();
+                    newEntry._promptText = prompt;
+                    newEntry._entryText = response;
+
+                    journal.AddEntry(newEntry);
+
+                    Console.WriteLine("Journal entry added.");
+                    Console.WriteLine("Press enter to continue...");
+                    Console.ReadLine();
+
+
+                    
                     // add entry to list of journal entries
                     break;
                 case 2:
                     // Display journal entries here
+
+                    Console.WriteLine();
+                    journal.DisplayAll();
+                    Console.WriteLine("Press enter to continue...");
+                    Console.ReadLine();
+
                     break;
                 case 3:
                     // save journal to file
+                    Console.WriteLine("Enter filename to save: ");
+                    string saveFile = Console.ReadLine();
+                    journal.SaveToFile(saveFile);
                     break;
                 case 4:
                     // load journal from file
+                    Console.WriteLine("Enter filename to load: ");
+                    string loadFile = Console.ReadLine();
+                    journal.LoadFromFile(loadFile);
+                    Console.WriteLine("Journal loaded.");
+                    Console.WriteLine("Press enter to continue...");
+                    Console.ReadLine();
+                    
                     break;
                 case 5:
                     done = true;
