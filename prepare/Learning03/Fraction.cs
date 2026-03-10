34066be1a78f238f1391edf4d500b941c56a5375
bradyung <ybrad1109@gmail.com>
2026-03-09 18:06:31 -0600

    Learning 3 working.

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,69 @@
+using System;
+
+public class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _numerator = wholeNumber;
+        _denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator)
+    {
+        _numerator = numerator;
+        SetDenominator(denominator);
+    }
+
+    public void SetNumerator(int numerator)
+    {
+        _numerator = numerator;
+    }
+
+    public void SetDenominator(int denominator)
+    {
+        if (denominator != 0)
+        {
+            _denominator = denominator;
+        }
+        else
+        {
+            _denominator = 1;
+        }
+    
+    }
+
+    public int GetNumerator()
+    {
+        return _numerator;
+    }
+
+    public int GetDenominator()
+    {
+        return _denominator;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / _denominator;
+    }
+
+
+
+    
+
+
+}
