521c12237ce87d6b2f90d27e13a4b8a7b82db926
bradyung <ybrad1109@gmail.com>
2026-03-10 17:00:05 -0600

    angle.cs working

---
+++ sandbox/Angle/Angle.cs
@@ -0,0 +1,48 @@
+
+
+using System.ComponentModel;
+
+class Angle
+{
+    private double _radians;
+
+    public Angle()
+    {
+        _radians = 0.0;
+    }
+
+    public Angle(double radians)
+    {
+        _radians = radians;
+    }
+
+    public Angle(int degrees)
+    {
+        _radians = ToRadians(degrees);
+    }
+
+    private double ToRadians(double degrees)
+    {
+        return degrees * Math.PI / 180.0;
+    }
+
+    public double GetRadians()
+    {
+        return _radians;
+    }
+
+    public double GetDegrees()
+    {
+        return ToDegrees();
+    }
+
+    private double ToDegrees()
+    {
+        return _radians / Math.PI * 180.0;
+    }
+
+    public void SetDegrees(int degrees)
+    {
+        _radians = ToRadians(degrees);
+    }
+}
