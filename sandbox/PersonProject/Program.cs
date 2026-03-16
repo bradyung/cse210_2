c15ab86a2eee9df82a0df6e1c96e50bbb0df2fa3
bradyung <ybrad1109@gmail.com>
2026-03-16 09:58:44 -0600

    person project working.

---
+++ sandbox/PersonProject/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Person person = new Person("Bob", "Ross", 55, 195);
+        Console.WriteLine(person.GetPersonInformation());
+        Doctor doctor = new Doctor("Axe", "Bob", "Budge", 75, 210);
+        Console.WriteLine(doctor.GetDoctorInformation());
+    }
+
+}
