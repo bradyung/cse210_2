d857f476131da2401388866c3d189fc972d355fe
bradyung <ybrad1109@gmail.com>
2026-03-26 09:30:06 -0600

    added SetDesciption and SetPoints

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -15,11 +15,21 @@ class Goal
     }
     public void SetName()
     {
-        Console.WriteLine("Please enther the name of your goal: ");
+        Console.Write("Please enther the name of your goal: ");
         _name = Console.ReadLine();
+    }
+    public void SetDescription()
+    {
+        Console.Write("Please enther the description of your goal: ");
+        _description = Console.ReadLine();
+    }
+    public void SetPoints()
+    {
+        Console.Write("Please enther the points of your goal: ");
+        _numberOfPoints = int.Parse(Console.ReadLine());
     }
     public virtual string GetConsoleString()
     {
-        return $"Goal information: {_name}";
+        return $"Goal information: {_name}, {_description}, {_numberOfPoints}";
     }
 }
